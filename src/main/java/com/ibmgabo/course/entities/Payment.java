04f9235eeb93eff212368e26aebad91e63ba6e17
Gabriel Teixeira Ricca <gabriel.teixeira.ricca@ibm.com>
2026-01-14 13:46:26 -0300

    Subtotal & Total methods

--- src/main/java/com/ibmgabo/course/entities/Payment.java
+++ src/main/java/com/ibmgabo/course/entities/Payment.java
@@ -4,10 +4,14 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 
@@ -22,6 +26,9 @@ public class Payment implements Serializable{
 	private Long id;
 	private Instant moment;
 	
+	@JsonIgnore
+	@OneToOne
+	@MapsId
 	private Order order;
 	
 	public Payment() {
