952535bce1014ae44471302e4f002495eae1f5ff
Gabriel Teixeira Ricca <gabriel.teixeira.ricca@ibm.com>
2026-01-14 11:02:41 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/ibmgabo/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.ibmgabo.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ibmgabo.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+}
